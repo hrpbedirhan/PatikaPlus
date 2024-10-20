ce575879eb9cfadb8d64d220615cebd10891221e
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-10-20 23:55:18 +0300

    method ödevi yapıldı..

---
+++ method-odev/Program.cs
@@ -0,0 +1,47 @@
+﻿// Geriye Değer Döndürmeyen Bir void metot
+using System.Reflection.Metadata;
+
+static void getSong()
+{
+    Console.WriteLine("Sen gelmez oldun...");
+}
+
+getSong();
+
+Console.WriteLine("-----------------------------");
+
+// Geriye Tamsayı Döndüren Bir metot
+static int randomNumber()
+{
+    Random rand = new Random();
+    int randomNumber = rand.Next();
+    randomNumber = randomNumber % 2;
+
+    return randomNumber;
+}
+
+int number =  randomNumber();
+Console.WriteLine(number);
+
+Console.WriteLine("-----------------------------");
+
+// Parametre Alan ve Geriye Değer Döndüren Bir Metot
+static int multiplication(int x, int y)
+{
+    return x * y;
+}
+
+int result = multiplication(3, 5);
+Console.WriteLine(result);
+
+Console.WriteLine("-----------------------------");
+
+// Parametre Alan ve Geriye Değer Döndürmeyen bir Metot
+static void greeting(string name , string surname)
+{
+    Console.WriteLine($"Hoş Geldiniz {name} {surname}");
+}
+
+greeting("Bedirhan", "Harputluoğlu");
+
+Console.ReadKey();
