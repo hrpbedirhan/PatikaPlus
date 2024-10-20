3915d9c25778cd8cfe089378e220fcc919482c03
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-10-20 13:08:23 +0300

    Add project files.

---
+++ if-else-odev/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Lütfen Bir Sayı Giriniz: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 10)
+    Console.WriteLine("Girilen Sayı 10'dan büyüktür.");
+else
+    Console.WriteLine("Girilen Sayı 10'dan küçüktür.");
+
+if (number % 2 == 0)
+    Console.WriteLine("Girilen Sayı Çifttir.");
+else
+    Console.WriteLine("Girilen Sayı tektir.");
+
+Console.ReadKey();
---
+++ switch-case-odev/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace switch_case_odev
+{
+    internal class Program
+    {
+        enum Fruits
+                {
+                    Elma = 2,
+                    Armut = 3,
+                    Cilek = 2,
+                    Muz = 3,
+                    Diger = 4
+                }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"Elma = {(int)Fruits.Elma} TL");
+            Console.WriteLine($"Armut = {(int)Fruits.Armut} TL");
+            Console.WriteLine($"Çilek= {(int)Fruits.Cilek} TL");
+            Console.WriteLine($"Muz = {(int)Fruits.Muz} TL");
+            Console.WriteLine($"Diger bütün meyveler = {(int)Fruits.Diger} TL");
+
+            Console.Write("Hangi Meyveyi satin almak istersiniz? (Elma/Armut/Çilek/Muz/Diger): ");
+            string fruit = Console.ReadLine();
+
+            switch (fruit.ToLower())
+            {
+                
+
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
