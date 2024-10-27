125603629d08a4a8258b13fbdfdf3021deef929e
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-10-28 01:03:14 +0300

    constructor ödevi yapıldı..

---
+++ ctor-odev/Baby.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctor_odev
+{
+    public class Baby
+    {
+        //the first constructor is parameterless
+        public Baby()
+        {
+            Console.WriteLine("Ingaaaa");
+            BornDate = DateTime.Now;
+        }
+
+        //the second constructor is parameter
+        public Baby(string name, string surname)
+        {
+            Console.WriteLine("Ingaaaa");
+            Name = name;
+            Surname = surname;
+            BornDate = DateTime.Now;
+        }
+
+        //proporties
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime BornDate { get; set; }
+
+        //print baby information
+        public void GetBabyInfo()
+        {
+            Console.WriteLine($"Ad: {Name}, Soyad: {Surname}, Doğum Tarihi: {BornDate}");
+        }
+    }
+}
---
+++ ctor-odev/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ctor_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Baby baby = new Baby();
+            baby.Name = "Esin";
+            baby.Surname = "Yalın";
+            baby.GetBabyInfo();
+
+            Console.WriteLine("------------------------");
+
+            Baby baby2 = new Baby("Şeyma", "Güler");
+            baby2.GetBabyInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
