78157496cd27f2fdcfe556ca54f9557ee90f2c92
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-10-29 16:23:52 +0300

    kütüphane ödevi yapıldı..

---
+++ kutuphane-odev/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kutuphane_odev
+{
+    internal class Book
+        //class bir yapıyı tanımlamak için kullanılan şablondur
+    {
+
+        public Book()
+        {
+            RegistrationDate = DateTime.Now;
+        }
+
+        public Book(string name, string writerName, string writerSurname, int pageNumber, string publishHouse)
+        {
+            Name = name;
+            WriterName = writerName;
+            WriterSurname = writerSurname;
+            PageNumber = pageNumber;
+            PublishHouse = publishHouse;
+            RegistrationDate = DateTime.Now;
+        }
+        //constructor sınıf örneği oluşturulurken çalışan özel bir metot
+
+
+        //property definition
+        public string Name { get; set; }
+        public string WriterName { get; set; }
+        public string WriterSurname { get; set; }
+        public int PageNumber { get; set; }
+        public string PublishHouse { get; set; }
+        private DateTime RegistrationDate { get; set; }
+        //property class alanlarını kapsülleyerek onlara kontrollü şekilde erişim için kullanılır
+
+
+        //kitap bilgilerini yazdıran metot void kelimesi metot hiçbir şey döndürmüyorsa eklenir
+        public void GetBookInfo()
+        {
+            Console.WriteLine($"Ad: {Name}, Yazar adı: {WriterName}, Yazar soyadı: {WriterSurname}, Sayfa Sayısı: {PageNumber}, Yayınevi {PublishHouse}, Kayıt Tarihi: {RegistrationDate}");
+        }
+    }
+}
---
+++ kutuphane-odev/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace kutuphane_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            //new anahtar kelimesi claas üzerinden yeni bir nesne oluşturmak için kullanılır
+            book1.Name = "Bir Kitap";
+            book1.WriterName = "Ahmet";
+            book1.WriterSurname = "Yılmaz";
+            book1.PageNumber = 320;
+            book1.PublishHouse = "Alfa Yayınları";
+
+            Book book2 = new Book("Başka Bir Kitap", "Ayşe", "Karan", 215, "Beta Yayıncılık");
+            Book book3 = new Book("Daha Başka Kitap", "Mehmet", "Demir", 450, "Gamma Yayınları");
+            Book book4 = new Book("Son Kitap", "Elif", "Şahin", 275, "Delta Yayıncılık");
+            Book book5 = new Book("Örnek Kitap", "Selin", "Yurt", 198, "Epsilon Yayınları");
+
+            book1.GetBookInfo();
+            Console.WriteLine("------------------------");
+            book2.GetBookInfo();
+            Console.WriteLine("------------------------");
+            book3.GetBookInfo();
+            Console.WriteLine("------------------------");
+            book4.GetBookInfo();
+            Console.WriteLine("------------------------");
+            book5.GetBookInfo();
+        }
+    }
+}
