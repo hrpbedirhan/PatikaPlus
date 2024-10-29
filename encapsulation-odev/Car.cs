b4c97db13ab9b9c2d8e7287cfce5af26c6578488
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-10-29 22:26:00 +0300

    encapsulation ödevi yapıldı..

---
+++ encapsulation-odev/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace encapsulation_odev
+{
+    internal class Car
+    {
+
+        //parametless constructor
+        public Car() {}
+
+        //parameters constructor
+        public Car(string brand, string model, string color, int doorsCount)
+        {
+            Brand = brand;
+            Model = model;
+            Color = color;
+            DoorCount = doorsCount;
+        }
+
+        //properties
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        private int _doorCount;
+        public int DoorCount
+        {
+            get { return _doorCount; }
+            set 
+            {
+                if (2 == value || 4 == value) //set control
+                {
+                    _doorCount = value;
+                }
+                else
+                {
+                    Console.WriteLine("Hata: Kapı sayısı sadece 2 veya 4 olabilir. Kapı sayısı -1 olarak ayarlandı.");
+                    _doorCount = -1;
+                }
+            }
+        }
+
+        //get car information
+        public void GetCarInfo()
+        {
+            Console.WriteLine($"Marka: {Brand}, Model: {Model}, Renk: {Color}, Kapı Sayısı: {DoorCount}");
+        }
+    }
+}
---
+++ encapsulation-odev/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace encapsulation_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Toyota", "Corolla", "Mavi", 4);
+            Car car2 = new Car("Honda", "Civic", "Beyaz", 3);
+            Car car3 = new Car("BMW", "5.20d", "Siyah", 4);
+            Car car4 = new Car("Fiat", "500L", "Kırmızı", 2);
+
+            car1.GetCarInfo();
+            Console.WriteLine("--------------------------");
+            car2.GetCarInfo();
+            Console.WriteLine("--------------------------");
+            car3.GetCarInfo();
+            Console.WriteLine("--------------------------");
+            car4.GetCarInfo();
+        }
+    }
+}
