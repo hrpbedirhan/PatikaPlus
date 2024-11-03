24d8a83069ec47260b9c9aaf72eba834bdd4f913
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-11-04 00:59:52 +0300

    list ödevi yapıldı..

---
+++ list-odev/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace list_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("** Davetliler **");
+
+            //created list
+            List<string> guests = new List<string>()
+            {
+                "Bülent Ersoy",
+                "Ajda Pekkan",
+                "Ebru Gündeş",
+                "Hadise",
+                "Hande Yener",
+                "Tarkan",
+                "Funda Arar",
+                "Demet Akalın"
+            };
+
+            //print the list elements
+            for (int i = 0; i < guests.Count; i++)
+            {
+                Console.WriteLine($"{i + 1} - {guests[i]}");
+            }
+        }
+    }
+}
