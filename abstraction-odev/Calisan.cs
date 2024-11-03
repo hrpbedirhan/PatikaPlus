9604988fc55b58a88e6786b66d1fa08cac375fa4
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-11-03 13:27:00 +0300

    abstraction ödevi yapıldı

---
+++ abstraction-odev/Calisan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstraction_odev
+{
+    public abstract class Calisan
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Departman { get; set; }
+
+        public abstract void Gorev();
+
+    }
+
+    public class YazilimGelistirici : Calisan
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} Yazılım geliştirici olarak çalışıyorum.");
+        }
+    }
+
+    public class ProjeYöneticisi : Calisan
+    {
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} Proje yöneticisi olarak çalışıyorum.");
+        }
+    }
+        public class SatisTemsilcisi : Calisan
+        {
+            public override void Gorev()
+            {
+                Console.WriteLine($"{Ad} {Soyad} Satış temsilcisi olarak çalışıyorum.");
+            }
+        }
+}
---
+++ abstraction-odev/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace abstraction_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ProjeYöneticisi projeYöneticisi = new ProjeYöneticisi { Ad = "Hasan", Soyad = "Çıldırmış", Departman = "Proje Yönetimi"};
+            projeYöneticisi.Gorev();
+
+            YazilimGelistirici yazilimGelistirici = new YazilimGelistirici { Ad="Burak", Soyad="Öz", Departman="Yazılım"};
+            yazilimGelistirici.Gorev();
+
+            SatisTemsilcisi satisTemsilcisi = new SatisTemsilcisi { Ad="Yaren", Soyad="Kara", Departman="Satış" };
+            satisTemsilcisi.Gorev();
+
+        }
+    }
+}
