7b0561c943d8ed6309c2390e26802b66d3ab7e0a
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-11-03 12:05:24 +0300

    polymorphism ödevi yapıldı

---
+++ polymorphism-odev/BaseGeometrikSekil.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace polymorphism_odev
+{
+    //base class
+    public class BaseGeometrikSekil
+    {
+        public double Genislik { get; set; }
+        public double Yukseklik { get; set; }
+
+        public virtual double AlanHesapla()
+        {
+            return Genislik * Yukseklik;
+        }
+    }
+
+    //square class
+    public class Kare : BaseGeometrikSekil
+    {
+        public override double AlanHesapla()
+        {
+            return base.AlanHesapla();
+        }
+    }
+
+    //rectangle class
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public override double AlanHesapla()
+        {
+            return base.AlanHesapla();
+        }
+    }
+
+    //triangle class
+    public class DikUcgen : BaseGeometrikSekil
+    {
+        public override double AlanHesapla()
+        {
+            return (Genislik*Yukseklik)/2;
+        }
+    }
+}
---
+++ polymorphism-odev/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace polymorphism_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //square
+            Kare kare = new Kare { Genislik = 10, Yukseklik = 10};
+            Console.WriteLine($"Kare alanı: {kare.AlanHesapla()}");
+
+            //rectangle
+            Dikdortgen dikdortgen = new Dikdortgen { Genislik = 20, Yukseklik = 12};
+            Console.WriteLine($"Dikdörgen alanı: {dikdortgen.AlanHesapla()}");
+
+            //triangle
+            DikUcgen dikUcgen = new DikUcgen { Genislik = 10, Yukseklik = 15 };
+            Console.WriteLine($"Dik Üçgen alanı: {dikUcgen.AlanHesapla()}");
+        }
+    }
+}
