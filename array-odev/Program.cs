eb54b61a31d095ec5d352ae810d9cb3f7844d7e8
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-11-04 00:41:07 +0300

    array ödevi yapıldı..

--- array-odev/Program.cs
+++ array-odev/Program.cs
@@ -5,27 +5,29 @@
         static void Main(string[] args)
         {
 
-            //boş bir liste oluşturduk
-            List<int> array = new List<int>();
+            //boş bir array oluşturduk
+            int[] array = new int[10];
 
             //ilk 10 elemanını for ile ekledik
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < array.Length; i++)
             {
-                array.Add(i + 1);
+                array[i] = i + 1;
             }
 
             //foreach ile elemanlarını yazdırdık
             foreach (int arr  in array)
                 Console.WriteLine(arr);
 
+            Array.Resize(ref array, 11);
+
             //11. elemanı kullanıcıdan aldık ve listeye ekledik
             Console.Write("11. elemanı giriniz: ");
             int newItem = Convert.ToInt32(Console.ReadLine());
-            array.Add(newItem);
+            array[10] = newItem;
 
             //listeyi sort metodu ile küçükten büyüğe sırayalıp reverse ile sıralamayı tersine çevirdik
-            array.Sort();
-            array.Reverse();
+            Array.Sort(array);
+            Array.Reverse(array);
 
             //listenin son halini foreach ile yazdırdık
             foreach (int arr in array)
