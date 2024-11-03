680682c00314fd9988430dd1805565d722503e73
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-11-04 01:13:38 +0300

    kahve listesi yapıldı..

---
+++ kahve-list-dev/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace kahve_list_dev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> coffee = new List<string>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"Kahve {i}");
+            }
+        }
+    }
+}
---
+++ kahve-list-odev/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace kahve_list_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //created list
+            List<string> coffee = new List<string>();
+
+            //add coffee
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write($"Kahve {i}: ");
+                string item = Console.ReadLine();
+                coffee.Add(item);
+            }
+
+            //print the coffe elements
+            Console.WriteLine("Girilen Kahve İsimleri:");
+            foreach (string c in coffee)
+                Console.WriteLine(c);
+        }
+    }
+}
