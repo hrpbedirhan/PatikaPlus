e5c5b9c2cd961c53d2838fd65a2d8277fe58b4c0
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-11-03 11:30:15 +0300

    inheritance ödevi yapıldı

---
+++ inheritance-odev/BaseKisi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inheritance_odev
+{
+    //miras alınacak kişi sınıfı
+    public class BaseKisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+
+        public void AdSoyadYazdir()
+        {
+            Console.WriteLine($"Ad: {Ad}, Soyad: {Soyad}");
+        }
+    }
+
+    //miras alan öğrenci sınıfı
+    public class Ogrenci : BaseKisi
+    {
+        public int OgrenciNumarasi { get; set; }
+
+        public void OgrenciBilgileriYazdir()
+        {
+            Console.WriteLine($"Öğrenci No: {OgrenciNumarasi}");
+            AdSoyadYazdir();
+        }
+    }
+
+    //miras alan öğretmen sınıfı
+    public class Ogretmen : BaseKisi 
+    {
+        public decimal Maas { get; set; }
+
+        public void OgretmenBilgileriYazdir()
+        {
+            Console.WriteLine($"Maaş: {Maas}");
+            AdSoyadYazdir();
+        }
+    }
+}
---
+++ inheritance-odev/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace inheritance_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //constructor tanımı yapmadan da süslü parantez ile nesnenin içine girip bu şekilde tanımlama yapabiliyoruz
+            Ogrenci ogrenci = new Ogrenci { Ad = "Ecrin", Soyad = "Çakır", OgrenciNumarasi = 12345};
+            ogrenci.OgrenciBilgileriYazdir();
+
+            Ogretmen ogretmen = new Ogretmen { Ad = "Bedirhan", Soyad = "Harputluoğlu", Maas = 20000 };
+            ogretmen.OgretmenBilgileriYazdir();
+        }
+    }
+}
