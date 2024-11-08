189749acf8e48b6dffb0cff70acdbc92484b665d
Bedirhan Harputuloglu <bedirhanharputlu@gmail.com>
2024-11-08 15:50:47 +0300

    try-catch ödevi yapıldı

---
+++ try-catch-odev/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace try_catch_odev
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Lütfen bir sayı giriniz: ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine($"Girdiğiniz sayının karesi: {Math.Pow(number, 2)}");
+            }
+            catch ( FormatException ) 
+            {
+                Console.WriteLine("Geçersiz giriş! Lütfen bir sayı giriniz.");
+            }
+        }
+    }
+}
