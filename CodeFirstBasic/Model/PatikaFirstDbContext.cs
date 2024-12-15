434663cdb547be5e47abbea17d4a78eb8f3d849f
Bedirhan Harputluoğlu <bedirhan-bomba@hotmail.com>
2024-12-16 00:41:48 +0300

    Code First Yaklaşımı ile Veri Tabanı Oluşturma

---
+++ CodeFirstBasic/Model/PatikaFirstDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CodeFirstBasic.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeFirstBasic.Model
+{
+    public class PatikaFirstDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=PatikaCodeFirstDb1;Username=postgres;Password=[Your_Password]");
+        }
+
+    }
+}
