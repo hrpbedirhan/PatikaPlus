522f1fb3d05ef4c330ab6f34737bdb3cea9c4b58
Bedirhan Harputluoğlu <bedirhan-bomba@hotmail.com>
2024-12-16 03:16:58 +0300

    Code First Relation Code First Yaklaşımı ile Veri Tabanı ve İlişkiler Oluşturma

---
+++ CodeFirstRelation/Model/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeFirstRelation.Model.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = "";
+        public string Email { get; set; } = "";
+
+        public ICollection<Post> Posts { get; set; }
+    }
+}
---
+++ CodeFirstRelation/Model/PatikaSecondDbContext.cs
@@ -0,0 +1,25 @@
+﻿using CodeFirstRelation.Model.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace CodeFirstRelation.Model
+{
+    public class PatikaSecondDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=PatikaCodeFirstDb2;Username=postgres;Password=your_password");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasOne(p => p.User)
+                .WithMany(p => p.Posts)
+                .HasForeignKey(p => p.UserId);
+        }
+    }
+}
